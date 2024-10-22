0dcdd274202f275b26ce3a3c5b02398c32111fd5
LucAsl14 <lucas.fu070807@gmail.com>
2024-10-22 11:46:19 -0400

    add funny(?) enemy behaviour

--- basic_enemy.gd
+++ basic_enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const JUMP_SPEED = -600.0
 var direction = 1
 
 func _ready():
@@ -10,14 +11,19 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 10 #bandage fix for slopes
+		velocity += get_gravity() * delta #bandage fix for slopes
 		
 	if (!$Hitbox/DownRay.is_colliding() or $Hitbox/SideRay.is_colliding()) and is_on_floor():
-		flip()
+		if ($Hitbox/SideRay.is_colliding()):
+			jump()
+		else: 
+			flip()
 		
 	velocity.x = direction * SPEED
 	move_and_slide() 
 
+func jump():
+	velocity.y = JUMP_SPEED
 
 func flip():
 	direction = -direction
