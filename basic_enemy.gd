841b7f54237c7f49612ba99849a8ea7df86e673b
LucAsl14 <lucas.fu070807@gmail.com>
2024-10-23 15:39:33 -0400

    new and improved ai (trust)

--- basic_enemy.gd
+++ basic_enemy.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_SPEED = -600.0
 var direction = 1
+var hasJumped = true;
 
 func _ready():
 	$AnimatedSprite2D.play("moving")
@@ -11,13 +12,22 @@ func _ready():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta #bandage fix for slopes
+		if (randi() % 20 == 1 && !hasJumped):
+			jump() # chance of jumping in air once
+			hasJumped = true
+		else:
+			velocity += get_gravity() * delta #bandage fix for slopes
+	else:
+		hasJumped = false;
 		
 	if (!$Hitbox/DownRay.is_colliding() or $Hitbox/SideRay.is_colliding()) and is_on_floor():
-		if ($Hitbox/SideRay.is_colliding()):
+		if ($Hitbox/SideRay.is_colliding() && (randi() % 2 == 0)):
 			jump()
 		else: 
-			flip()
+			if (randi() % 5 != 0):
+				flip()
+			else:
+				jump()
 		
 	velocity.x = direction * SPEED
 	move_and_slide() 
