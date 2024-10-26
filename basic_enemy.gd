51ef28741d323eca293b7edf4f454be4e9da8a58
And E <348726067@gapps.yrdsb.ca>
2024-10-26 17:14:31 -0400

    Animations and killing the enemy

--- basic_enemy.gd
+++ basic_enemy.gd
@@ -40,3 +40,8 @@ func jump():
 func flip():
 	direction = -direction
 	scale.x *= -1
+
+
+func _on_killbox_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if(body.is_in_group("player") && body.velocity.y > 0):
+		queue_free()
