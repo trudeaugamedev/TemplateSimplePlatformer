229b700efda4dbebb072079aa43824d5339cc097
And E <348726067@gapps.yrdsb.ca>
2024-10-26 15:44:30 -0400

    Added instant death

--- checkpoint.gd
+++ checkpoint.gd
@@ -5,4 +5,4 @@ extends Node2D
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if(body.is_in_group("player")):
-		spawn.position = tilemap.map_to_local(tilemap.local_to_map(position))
+		spawn.position = position
